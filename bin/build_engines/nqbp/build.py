46ca0d6a901b3d443a92aff66c5f915935bf34a6
john <john.t.taylor@gmail.com>
2020-05-17 16:52:20 -0600

    add more filter options to bob

--- bin/build_engines/nqbp/build.py
+++ bin/build_engines/nqbp/build.py
@@ -9,15 +9,26 @@ usage: build [options] here [<opts>...]
 Options:
     here                 Builds all NQBP projects starting from the current 
                          working directory.
+    <opts>               Option(s) to be passed directly to NQBP
     PATTERN              If a subdir under PRJDIR matches PATTERN, then that
                          directory is built.  Standard Python wildcards can
                          be used in the PATTERN.
-    --filter1 FILTER1    FILTER1 is and'd with PATTERN to determine what
-                         directories are built.
+    --p2 FILTER          An second filter that is AND'd with PATTERN to
+                         filter which sub-directories get built. Standard Python 
+                         wildcards can be used in the FILTER.
+    --p3 FILTER          An third filter that is AND'd with PATTERN to
+                         filter which sub-directories get built. Standard Python 
+                         wildcards can be used in the FILTER.
     --path PRJDIR        The full path to the project directory of the 
                          projects to build.  If no path is specified, the
                          current working directory is used for the project
                          path.
+    --exclude DIR        Excludes the specified sub directory. Standard Python 
+                         wildcards can be used in the DIR
+    --e2 DIR             Excludes an additional sub directory. Standard Python 
+                         wildcards can be used in the DIR
+    --e3 DIR             Excludes an additional sub directory. Standard Python 
+                         wildcards can be used in the DIR
     --file BLDLIST       A text file containing a list of projects to build.
                          The format of file is a list of 'build' commands. 
                          Blank lines and line starting with '#' are skipped.
@@ -114,25 +125,31 @@ def run( common_args, cmd_argv ):
         if ( args['here'] ):
             pattern = '*'
     
-        # Apply pattern/directories filter(s)
-        prj_list = _filter_prj_list( all_prjs, pattern, pkgroot )
-        if ( args['--filter1'] ):
-            prj_list = _filter_prj_list( prj_list, args['--filter1'], pkgroot )
-
-        for p in prj_list:
+        # Apply the include/exclude filters
+        jobs = _filter_prj_list( all_prjs, pattern, pkgroot, args['--exclude'], args['--e2'], args['--e3'], args['--p2'], args['--p3'] )
+       
+        # Run the Jobs serially
+        for p in jobs:
             _build_project(p, args['-v'], args['<opts>'], args['--config'], args['--xconfig'], pkgroot )
 
     # restore original cwd
     utils.pop_dir()
     
 #------------------------------------------------------------------------------
-def _filter_prj_list( all_prj, pattern, pkgroot ):
+def _filter_prj_list( all_prj, pattern, pkgroot, exclude=None, exclude2=None, exclude3=None, p2=None, p3=None ):
     list = []
     for p in all_prj:
         relpath = p.replace(pkgroot,'')
         dirs    = relpath.split(os.sep)
         if ( len(fnmatch.filter(dirs,pattern))> 0 ):
-            list.append( p )
+            keep1 = True if exclude == None or len(fnmatch.filter(dirs,exclude)) == 0 else False
+            keep2 = True if exclude2 == None or len(fnmatch.filter(dirs,exclude2)) == 0 else False
+            keep3 = True if exclude3 == None or len(fnmatch.filter(dirs,exclude3)) == 0 else False
+            keep4 = True if p2 == None or len(fnmatch.filter(dirs,p2)) > 0 else False
+            keep5 = True if p3 == None or len(fnmatch.filter(dirs,p3)) > 0 else False
+            if ( keep1 and keep2 and keep3 and keep4 and keep5 ):
+                list.append( p )
+            
 
     return list
     
@@ -144,7 +161,6 @@ def _build_project( prjdir, verbose, bldopts, config, xconfig, pkgroot ):
     elif ( xconfig ):
         cfg = xconfig
     
-    # Build the project
     utils.push_dir( os.path.dirname(prjdir) )
     print("BUILDING: "+ prjdir)
     cmd = 'nqbp.py ' + " ".join(bldopts)
@@ -152,7 +168,6 @@ def _build_project( prjdir, verbose, bldopts, config, xconfig, pkgroot ):
         cmd = utils.concatenate_commands( cfg, cmd )
     utils.run_shell( cmd, verbose, "ERROR: Build failure ({})".format(cmd) )
     utils.pop_dir()
-
 
 
 #-----------------------------------------------------------------------------
